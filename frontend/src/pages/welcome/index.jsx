626494cd5f817b17a74d3ac86c3a57446f7d75af
cherry2250 <kcr7812@naver.com>
2024-02-11 21:19:02 +0900

    Feat : 로그인 애니메이션(#5)

--- frontend/src/pages/welcome/index.jsx
+++ frontend/src/pages/welcome/index.jsx
@@ -1,14 +1,32 @@
 import logo from "../../assets/welcome/main.png";
 import team from "../../assets/welcome/c2ri.png";
+import copy from "../../assets/welcome/001.png";
+import kakaoLogin from "../../assets/welcome/kakaoLogin.png";
+import googleLogin from "../../assets/welcome/googleLogin.png";
 import "./styles.css";
 
 function welcome() {
   return (
     <div className="Welcome">
-      <h1>셔를레이호</h1>
-      <div>
+      <div className="fadeout">
+        <img className="main-logo" alt="iPhone_01" src={copy} />
         <img className="main-logo" alt="iPhone_01" src={logo} />
         <img className="team-logo" alt="iPhone_01" src={team} />
+      </div>
+
+      <div className="fadein">
+        <img className="main-logo" alt="iPhone_01" src={copy} />
+        <div className="login-module">
+          <button>
+            <img className="login" alt="iPhone_01" src={kakaoLogin} />
+          </button>
+          <button>
+            <img className="login" alt="iPhone_01" src={googleLogin} />
+          </button>
+          <button>
+            <div>로그인 없이 둘러보기</div>
+          </button>
+        </div>
       </div>
     </div>
   );
