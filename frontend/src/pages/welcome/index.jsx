f26903f7b631c31f42e57e4ce4c18b055f5a2dc1
cherry2250 <kcr7812@naver.com>
2024-02-12 17:17:37 +0900

    Feat : 로그인버튼(#5)

--- frontend/src/pages/welcome/index.jsx
+++ frontend/src/pages/welcome/index.jsx
@@ -1,3 +1,4 @@
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import logo from "../../assets/welcome/main.png";
 import team from "../../assets/welcome/c2ri.png";
 import copy from "../../assets/welcome/001.png";
@@ -7,25 +8,29 @@ import "./styles.css";
 
 function welcome() {
   return (
-    <div className="Welcome">
-      <div className="fadein">
-        {/* <img className="main-logo" alt="iPhone_01" src={copy} /> */}
+    <div className="fadein">
+      <div>
+        <img className="main-copy" src={copy} alt="" />
+      </div>
+      <div className="sub-copy">
+        <p>더 간편하게 이용하는 출퇴근 셔틀버스</p>
+      </div>
+      <div className="logo">
         <img className="main-logo" alt="iPhone_01" src={logo} />
       </div>
-
-      <div className="fadein">
-        <div className="login-module">
-          <div className="login-button">
-            <img className="login" alt="iPhone_01" src={kakaoLogin} />
-          </div>
-          <div className="login-button">
-            <img className="login" alt="iPhone_01" src={googleLogin} />
-          </div>
-          <div className="login-button">
+      <div className="login-module">
+        <div className="login-button">
+          <img className="login" alt="iPhone_01" src={kakaoLogin} />
+        </div>
+        <div className="login-button">
+          <img className="login" alt="iPhone_01" src={googleLogin} />
+        </div>
+        <div className="login-button">
+          <Link to="/" className="login">
             <div className="login" id="no-login">
               로그인 없이 둘러보기
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
