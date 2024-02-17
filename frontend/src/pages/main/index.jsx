d75ff4d8591c57ea3cf9b56c38d108b2feb8f216
cherry2250 <kcr7812@naver.com>
2024-02-17 22:13:56 +0900

    Feat : main Recomm (#7)

--- frontend/src/pages/main/index.jsx
+++ frontend/src/pages/main/index.jsx
@@ -1,12 +1,12 @@
 // import logo from "../src/assets/logo.svg";
-import TestCompo from "../../components/Main/TestCompo";
+import MainRecomm from "../../components/Main/Recommendation";
+import MainSelect from "../../components/Main/Select";
 
 function Main() {
   return (
-    <div className="App">
-      <header className="App-header">노선정보 - 메인페이지</header>
-      <TestCompo ride="firstway"></TestCompo>
-      <button>버튼</button>
+    <div>
+      <MainSelect></MainSelect>
+      <MainRecomm></MainRecomm>
     </div>
   );
 }
