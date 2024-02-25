c7fb800c1be3be5ae86a89742f945f9c8a90c361
cherry2250 <kcr7812@naver.com>
2024-02-25 11:57:37 +0900

    Feat : 중간 커밋(#7)

--- frontend/src/apis/welcome.js
+++ frontend/src/apis/welcome.js
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export const loginApi = async () => {
-  const url = "http://13.124.246.186:8080/test";
-
+  const url = `/test`;
   try {
     const res = await axios({
       method: "get",
       url,
     });
-    // console.log(res);
+    console.log(res);
     return res;
   } catch (error) {
     console.log(error);
+    window.alert("fuck");
   }
 };
--- frontend/src/components/welcome/LoginModule/index.jsx
+++ frontend/src/components/welcome/LoginModule/index.jsx
@@ -4,15 +4,27 @@ import GoogleLogin from "../../../assets/Welcome/GoogleLogin.png";
 import { loginApi } from "../../../apis/Welcome";
 import "./styles.css";
 import "../../../styles/common.css";
+import axios from "axios";
+import React, { state, useState } from "react";
 
 function LoginModule() {
+  state = {
+    computers: [],
+  };
+
+  function componentDidMount() {
+    axios.get(`/test`).then((res) => {
+      const computers = res.data;
+      console.log(computers);
+    });
+  }
   return (
     <div className="fade-in">
       <div className="login-module">
         <div className="login-button" onClick={loginApi}>
           <img className="login" alt="Kakao Login" src={KakaoLogin} />
         </div>
-        <div className="login-button" onClick={loginApi}>
+        <div className="login-button" onClick={componentDidMount}>
           <img className="login" alt="Google Login" src={GoogleLogin} />
         </div>
         <div className="login-button" onClick={loginApi}>
